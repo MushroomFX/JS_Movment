661799d64cfded52ffce2797a1dbcb82e829d3ef
MushroomFX <dergoelm26@gmail.com>
2022-05-19 08:36:20 +0200

    init

--- movmentCore.js
+++ movmentCore.js
@@ -1,4 +1,18 @@
 var MFX = {
     setPlayer : function(input){},
     removePlayer : function(input){},
+    init: function(){
+        document.addEventListener("keydown", KeyDown);
+        document.addEventListener("keyup", KeyUp);
+
+        function KeyDown(e) {
+            localStorage.setItem(e.code, "true");
+            localStorage.setItem("last", e.code);
+        }
+        function KeyUp(e) {
+            localStorage.setItem(e.code, "false");
+            localStorage.setItem("last", e.code);
+        }
+    }
 }
+MFX.init()
