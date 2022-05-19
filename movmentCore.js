57672976d449f56686a7fafd3431dad7b9336228
MushroomFX <dergoelm26@gmail.com>
2022-05-19 09:46:24 +0200

    init

--- movmentCore.js
+++ movmentCore.js
@@ -1,6 +1,21 @@
+
 var MFX = {
-    setPlayer : function(input){},
-    removePlayer : function(input){},
+    config:{
+        speed:{
+            max:250,
+            acc:10,
+        }
+    },
+
+    player:{
+        set : function(input){
+            input.classList.add("mfx_player")
+        },
+        remove : function(input){
+            input.classList.remove("mfx_player")
+        }
+    },
+        
     init: function(){
         document.addEventListener("keydown", KeyDown);
         document.addEventListener("keyup", KeyUp);
@@ -12,7 +27,20 @@ var MFX = {
         function KeyUp(e) {
             localStorage.setItem(e.code, "false");
             localStorage.setItem("last", e.code);
+            console.log(e.code)
         }
-    }
+    },
+    updateLoop: setInterval(function(){
+        const  iputs = ["KeyW","KeyA","KeyS","KeyD"]
+        var input_table = []
+
+
+        for(i=0;i<inputs.length;i++){
+            input_table.push(localStorage.getItem(inputs[i]))
+        }
+        
+        for(i=0;i<document.getElementsByClassName('mfx_player').length;i++){
+        }
+    },1),
 }
 MFX.init()
