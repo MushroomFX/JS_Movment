6a25a788edf0b42c4910f44a1e50af4b62a5b6b7
MushroomFX <dergoelm26@gmail.com>
2022-05-19 13:46:47 +0200

    update without shift / ctrl yet

--- movmentCore.js
+++ movmentCore.js
@@ -3,9 +3,13 @@ var MFX = {
       speed: {
         max: 250, //max speed (obviuos i guess)
         acc: 10, //acceleration
-        dec: 10 //deceleration
+        dec: 5, //deceleration
+        smoothing: 100, //divides the end pos useing this
+        boost:10, //boost from normal speed in percent
+        sneak:10, //percent of normal speed
       },
-      inputs: ["KeyW", "KeyD", "KeyS", "KeyA"]
+      inputs: ["KeyW", "KeyD", "KeyS", "KeyA"],
+      mod_keys: [""]
     },
   
     player: {
@@ -30,23 +34,43 @@ var MFX = {
         localStorage.setItem("last", e.code);
       }
       var timeout = setTimeout(function () {
+        for (i = 0;i < document.getElementsByClassName("mfx_player").length;i++) {
+            var elem = document.getElementsByClassName("mfx_player")[i]
+                elem.value = [
+                    { x: 100, y: 100 },
+                    { x: 0, y: 0 }
+                ]
+        }
         const inputs = MFX.config.inputs;
         for (i = 0; i < inputs.length; i++) {
           var temp = `Move${inputs[i]}`;
           localStorage.setItem(temp, 10);
         }
       }, 100);
+      document.body.innerHTML += `
+      <style>
+        .test{
+            position: absolute;
+            background:blue;
+            color:transparent;
+            width:1em;
+            height:1em;
+            /*
+            -webkit-transition: all 0.125s 0s cubic-bezier(0,.2,0,1);
+            -moz-transition: all 0.125s 0s cubic-bezier(0,.2,0,1);
+            -o-transition: all 0.125s 0s cubic-bezier(0,.2,0,1);
+            transition: all 0.125s 0s cubic-bezier(0,.2,0,1);
+            */
+        }
+      </style>
+      `
     },
     updates: {
     updatePos:  function(){
-        for (
-            i = 0;
-            i < document.getElementsByClassName("mfx_player").length;
-            i++
-          ) {
+        for (i = 0;i < document.getElementsByClassName("mfx_player").length;i++) {
             var elem = document.getElementsByClassName("mfx_player")[i]
-            elem.style.bottom = elem.value[1].x +"px"
-            elem.style.left = elem.value[1].y +"px"
+            elem.style.bottom = elem.value[0].x +"px"
+            elem.style.left = elem.value[0].y +"px"
           }
     },
       updateLoop: setInterval(function () {
@@ -75,16 +99,14 @@ var MFX = {
         }
         var vel_x = ls("MoveKeyW") - ls("MoveKeyS");
         var vel_y = ls("MoveKeyD") - ls("MoveKeyA");
-        for (
-          i = 0;
-          i < document.getElementsByClassName("mfx_player").length;
-          i++
-        ) {
-          document.getElementsByClassName("mfx_player")[i].value = [
-            "x",
+        for (i = 0;i < document.getElementsByClassName("mfx_player").length;i++) {
+            var elem = document.getElementsByClassName("mfx_player")[i]
+            pos_x = elem.value[0].x + (vel_x / MFX.config.speed.smoothing)
+            pos_y = elem.value[0].y + (vel_y / MFX.config.speed.smoothing)
+          elem.value = [
+            { x: pos_x, y: pos_y },
             { x: vel_x, y: vel_y }
           ];
-        //   console.log(document.getElementsByClassName("mfx_player")[i].value[1]);
         }
         MFX.updates.updatePos()
     
